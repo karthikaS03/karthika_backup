59d8c9827f72bbcdd04493fb088c33b8d084228d
karthika subramani <ks54471@uga.edu>
2022-02-01 01:38:44 -0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -13,16 +13,14 @@ self.addEventListener('fetch', event => {
     if (cachedResponse) {     
       return cachedResponse;
     }
-//     if (event.request.url.indexOf('imgur')>0){
-//         // If we didn't find a match in the cache, use the network.
-//       var response = await fetch(event.request.url, {method:'POST', mode:'no-cors'})
-//     }
-//     else{
-     
+    if (event.request.url.indexOf('imgur')>0){
+       console.log('error')
+    }
+    else{     
      var response = await fetch(event.request)
      var res = response.clone()
-//     }
-    cache.put(event.request, response)
-    return res;
+     cache.put(event.request, response)
+      return res;
+     }  
   }());
 });
