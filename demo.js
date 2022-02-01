d4950fc73e12d68bbe5ef4cb8186c9b5277a68aa
karthika subramani <ks54471@uga.edu>
2022-02-01 01:37:19 -0500

    Update demo.js

--- demo.js
+++ demo.js
@@ -1,14 +1,16 @@
   console.log('Helo script')
 
+async function store_image(){
+  
   const request = new Request('https://i.imgur.com/fHyEMsl.jpg', {method: 'GET', mode:'no-cors'})
 
-  fetch(request)
-    .then(async function(response) {	
-      var cache = await caches.open('dynamic-v1');    	
-      await cache.put(request, response)
-  })
-
+  var response = await fetch(request)
+  var cache = await caches.open('dynamic-v1');    	
+  await cache.put(request, response)
+  
   if ('serviceWorker' in navigator) {              
       navigator.serviceWorker.register('sw.js');
 
   }
+}
+  
